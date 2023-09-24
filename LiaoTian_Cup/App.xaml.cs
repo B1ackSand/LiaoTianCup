b75587d92293763e7fa9e0f5896d191146cbc24b
B1ackSand <peilanfengfeng@gmail.com>
2023-09-24 13:36:12 +0800

    add log.

--- LiaoTian_Cup/App.xaml.cs
+++ LiaoTian_Cup/App.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using LiaoTian_Cup.Helper;
+using log4net;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +15,85 @@ namespace LiaoTian_Cup
     /// </summary>
     public partial class App : Application
     {
+        private static readonly ILog log = LogManager.GetLogger(typeof(App));
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            LogHelper.InitLog4Net();
+            RegisterEvents();
+            base.OnStartup(e);
+        }
+
+        private void RegisterEvents()
+        {
+            //Task线程内未捕获异常处理事件
+            TaskScheduler.UnobservedTaskException += TaskScheduler_UnobservedTaskException;
+
+            //UI线程未捕获异常处理事件（UI主线程）
+            DispatcherUnhandledException += App_DispatcherUnhandledException;
+
+            //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        //Task线程内未捕获异常处理事件
+        private static void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            try
+            {
+                if (e.Exception is Exception exception)
+                {
+                    HandleException(exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                e.SetObserved();
+            }
+        }
+
+        //非UI线程未捕获异常处理事件(例如自己创建的一个子线程)
+        private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                if (e.ExceptionObject is Exception exception)
+                {
+                    HandleException(exception);
+                }
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+        }
+
+        //UI线程未捕获异常处理事件（UI主线程）
+        private static void App_DispatcherUnhandledException(object sender, System.Windows.Threading.DispatcherUnhandledExceptionEventArgs e)
+        {
+            try
+            {
+                HandleException(e.Exception);
+            }
+            catch (Exception ex)
+            {
+                HandleException(ex);
+            }
+            finally
+            {
+                e.Handled = true;
+            }
+        }
+
+        //日志记录
+        private static void HandleException(Exception ex)
+        {
+            //记录堆栈和错误日志
+            LogHelper.WriteInfoLog(ex.ToString());
+            Current.Shutdown();
+        }
     }
 }
--- LiaoTian_Cup/Helper/CSVKit.cs
+++ LiaoTian_Cup/Helper/CSVKit.cs
@@ -8,13 +8,14 @@ namespace LiaoTian_Cup.Helper
 {
     class CSVKit
     {
+        // TODO 读写锁的问题存在
         //两个实现
         public static List<string[]> Csv2Dt(string filePath, List<string[]> list)
         {
             try
             {
                 StreamReader reader = new StreamReader(filePath, Encoding.UTF8, false);
-                while (reader.Peek()>0)
+                while (reader.Peek() > 0)
                 {
                     string str = reader.ReadLine();
                     string[] split = str.Split(',');
